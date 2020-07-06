b6bc176600ef6560d9063c9c9f119b08643b9e75
rik <rikjbouwmeester@gmail.com>
2020-07-06 11:11:54 +0200

    initial commit

---
+++ kalman.py
@@ -0,0 +1,328 @@
+import numpy as np
+from scipy.io import loadmat
+from scipy.signal import cont2discrete
+from scipy.linalg import expm
+import matplotlib.pyplot as plt
+
+data = loadmat('F16traindata_CMabV_2020.mat')
+z_k = np.transpose(data['Z_k'])
+c_m = data['Cm'][:, 0]
+u_k = np.transpose(data['U_k'])
+
+alpha_m = z_k[0,:]
+beta_m = z_k[1,:]
+v_m = z_k[2,:]
+
+Au = u_k[0,:]
+Av = u_k[1,:]
+Aw = u_k[2,:]
+
+
+def plot_data():
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.view_init(90,0)
+    ax.plot_trisurf(alpha_m, beta_m, c_m, cmap=cm.jet)
+    ax.set_xlabel(r'$\alpha_m$')
+    ax.set_ylabel(r'$\beta_m$')
+    ax.set_zlabel(r'$V_{tot}$')
+    plt.show()
+
+#
+# def rk4_step(f, x, t, dt, *args):
+#     """
+#     One step of 4th Order Runge-Kutta method
+#     """
+#     k = dt
+#     k1 = k * f(t, x, *args)
+#     k2 = k * f(t + 0.5*k, x + 0.5*k1, *args)
+#     k3 = k * f(t + 0.5*k, x + 0.5*k2, *args)
+#     k4 = k * f(t + dt, x + k3, *args)
+#     return x + 1/6. * (k1 + 2*k2 + 2*k3 + k4)
+#
+#
+# def rk4(f, t, x0, *args):
+#     """
+#     4th Order Runge-Kutta method
+#     """
+#     n = len(t)
+#     x = np.zeros((n, len(x0)))
+#     x[0] = x0
+#     for i in range(n-1):
+#         dt = t[i+1] - t[i]
+#         x[i+1] = rk4_step(f, x[i], t[i], dt, *args)
+#     return x
+
+
+def rk4(f, x, u, t):
+    """
+    4th Order Runge-Kutta method
+    """
+    h = (t[1] - t[0]) / 2
+    N = (t[1] - t[0]) / h
+
+    for j in range(0, int(N)):
+        k1 = h * f(x, u, t[0] + j * h)
+        k2 = h * f(x + k1 / 2, u, t[0] + j * h + h / 2)
+        k3 = h * f(x + k2 / 2, u, t[0] + j * h + h / 2)
+        k4 = h * f(x + k3, u, t[0] + j * h + h)
+
+        x = x + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+
+    return [t, x]
+
+
+def f(x, u, t):
+    """
+    System dynamics
+    """
+    x_dot = np.zeros(len(x))
+    np.put(x_dot, [0, 1, 2], u)
+    return x_dot
+
+
+def h(x):
+    """
+    Output dynamics
+    """
+    u = x[0]
+    v = x[1]
+    w = x[2]
+    c_alpha_up = x[3]
+    z_pred = np.zeros((3))
+
+    z_pred[0] = np.arctan2(w,u) * (c_alpha_up+1)
+    z_pred[1] = np.arctan2(v, np.sqrt(u**2 + w**2))
+    z_pred[2] = np.sqrt(u**2 + v**2 + w**2)
+    return z_pred
+
+
+# def F(x, u, t):
+#     """
+#     Calculate Jacobian of f
+#     """
+#     return np.zeros([4, 4])
+
+
+def jacobian_h(t, x, u):
+    #     """
+    #     Calculate Jacobian of h, H
+    #     """
+    jacobian = np.zeros((3,4))
+    u = x[0]
+    v = x[1]
+    w = x[2]
+    c_alpha_up = x[3]
+    jacobian[0,0] = -(c_alpha_up + 1) * w/(u**2+w**2)
+    jacobian[0,1] = 0.
+    jacobian[0,2] = (c_alpha_up + 1) * u/(u**2+w**2)
+    jacobian[0,3] = np.arctan2(w,u)
+    jacobian[1,0] = -((u*v)/(np.sqrt(u**2 + w**2) * (u**2 + v**2 + w**2)))
+    jacobian[1,1] = np.sqrt(u**2 + w**2)/(u**2 + v**2 + w**2)
+    jacobian[1,2] = -((v*w)/(np.sqrt(u**2 + w**2) * (u**2 + v**2 + w**2)))
+    jacobian[1,3] = 0.
+    jacobian[2,0] = u / np.sqrt(u**2 + v**2 + w**2)
+    jacobian[2,1] = v / np.sqrt(u**2 + v**2 + w**2)
+    jacobian[2,2] = w / np.sqrt(u**2 + v**2 + w**2)
+    jacobian[2,3] = 0.
+    return jacobian
+
+
+def c2d(a, b, t):  # todo; make own c2d function
+    """Continuous to discrete, based on MATLAB code"""
+    n  = np.size(a, 1)
+    nb = np.size(b, 1)
+    temp1 = np.concatenate((a, b), axis=1)*t
+    temp2 = np.zeros((nb,n+nb))
+
+    temp = np.concatenate((temp1, temp2), 0)
+
+    s = expm(temp)
+    Phi = s[0:n,0:n]
+    Gamma = s[0:n,n:n+nb]
+    return Phi, Gamma
+
+
+dt = 0.01  # time step
+N = len(c_m)  # data length
+n = 4  # number of states
+nm = 3  # number of measurements
+m = 3  # number of inputs
+
+B = np.vstack((np.identity(3), np.zeros(3)))  # input matrix
+G = np.identity(n)  # noise input matrix
+
+sigma_w = [1E-3, 1E-3, 1E-3, 0]
+Q = np.diag(np.square(sigma_w))
+
+sigma_v = [0.035, 0.013, 0.110]
+R = np.diag(np.square(sigma_v))
+
+sigma_x0 = [2.0, 2.0, 2.0, 50.0]
+P_0 = np.diag(np.square(sigma_x0))  # initial guess
+
+Ex_0 = [150., 0., 0., 0.]  # initial guess
+
+# =========================
+
+XX_k1k1 = np.zeros((n, N))
+PP_k1k1 = np.zeros((n, N))
+SIGMA_x_cor = np.zeros((n, N))
+z_pred = np.zeros((nm, N))
+IEKFitcount = np.zeros(N)
+# epsilon = 1e-12
+# doIEKF = 1
+# do_plot = 0
+# maxIterations = 2
+T = np.zeros((1,N))
+
+x_k_1k_1 = Ex_0  # assign initial guess
+P_k_1k_1 = P_0  # assign initial guess
+
+ti = 0
+tf = dt
+
+U_k = u_k
+Z_k = z_k
+
+for k in range(0, N):
+    # One-step ahead prediction
+    [t, x_kk_1] = rk4(f, x_k_1k_1, U_k[:,k], [ti, tf])
+    T[:,k] = t[0]  # alternatively; take the center value?
+
+    z_kk_1 = h(x_kk_1)
+    z_pred[:,k] = z_kk_1
+
+    # Calculate Jacobians
+    F = np.zeros([4, 4])
+    H = jacobian_h(0, x_kk_1, U_k[:, k])
+
+    # Discretize state transition & input matrices
+    [dummy, Psi] = c2d(F, B, dt)
+    [Phi, Gamma] = c2d(F, G, dt)
+
+    # Cov. matrix of state pred. error
+    P_kk_1 = Phi * P_k_1k_1 * Phi.T + Gamma * Q * Gamma.T
+    sigma_x_pred = np.sqrt(np.diag(P_kk_1))
+
+    # Kalman gain calculation
+    K = P_kk_1.dot(H.T).dot(np.linalg.inv(H.dot(P_kk_1).dot(H.T) + R))
+
+    # Measurement update
+    # x_k_1k_1 = x_kk_1 + K.dot(z_kk_1 - h(x_kk_1))
+    x_k_1k_1 = x_kk_1 + K.dot(Z_k[:, k] - z_kk_1)
+    # todo; iterated
+
+    # Cov. matrix of state estimation error
+    P_k_1k_1 = (np.identity(n) - K.dot(H)).dot(P_kk_1)
+    sigma_x_cor = np.sqrt(np.diag(P_k_1k_1))
+    SIGMA_x_cor[:, k] = sigma_x_cor
+
+    # Time step
+    ti = np.round(tf, 2)
+    tf = np.round(tf + dt, 2)
+
+    # todo; check rank of observation matrix, must be full rank, for sensor fusion
+    # todo; analysis of state observability. Derivative of derivative.
+    # todo; define matrix using sympy Matrix([udot])
+
+    XX_k1k1[:, k] = x_k_1k_1
+print(XX_k1k1[:, -1])
+do_plot = 1
+if do_plot:
+    start = 0
+    end = -1
+
+    # plt.close("all")
+    # plt.plot(XX_k1k1[0,:]-X_k[0,:])
+
+    # plt.figure(dpi=300)
+    # plt.title(r'$XX_{k1k1}$')
+    # plt.plot(T[0, start:end], np.transpose(XX_k1k1[0, start:end]), label=r'$u$')
+    # plt.plot(T[0, start:end], np.transpose(XX_k1k1[1, start:end]), label=r'$v$')
+    # plt.plot(T[0, start:end], np.transpose(XX_k1k1[2, start:end]), label=r'$w$')
+    # plt.plot(T[0, start:end], np.transpose(XX_k1k1[3, start:end]), label=r'$C_{\alpha_{up}}$')
+    # plt.xlabel(r'$Time\,[s]$')
+    # plt.legend()
+    # plt.show()
+    #
+    # plt.figure(dpi=300)
+    # plt.title(r'$STD_{cross-correlation}$')
+    # plt.plot(T[0, 0:500], np.transpose(SIGMA_x_cor[0, 0:500]), label=r'$u$')
+    # plt.plot(T[0, 0:500], np.transpose(SIGMA_x_cor[1, 0:500]), label=r'$v$')
+    # plt.plot(T[0, 0:500], np.transpose(SIGMA_x_cor[2, 0:500]), label=r'$w$')
+    # plt.plot(T[0, 0:500], np.transpose(SIGMA_x_cor[3, 0:500]), label=r'$C_{\alpha_{up}}$')
+    # plt.ylim([-0.5, 3.5])
+    # plt.xlabel(r'$Time\,[s]$')
+    # plt.ylabel(r'$\beta\,[rad]$')
+    # plt.legend()
+    # plt.show()
+    #
+    # plt.figure(dpi=300)
+    # plt.title(r'$C_{\alpha_{up}}$')
+    # plt.plot(T[0, start:end], XX_k1k1[3, start:end])
+    # plt.xlabel(r'$Time\,[s]$')
+    # plt.ylabel(r'$C_{\alpha_{up}}\,[-]$')
+    # # plt.legend()
+    # plt.show()
+
+    plt.figure(dpi=300)
+    plt.title(r'$Predicted\,vs\,Measured\,Output$')
+    plt.plot(T[0, start:end], np.transpose(z_k[0, start:end]), 'g', label=r'$\alpha_m$')
+    plt.plot(T[0, start:end], np.transpose(z_pred[0, start:end]), color='r', linewidth=2, label=r'$\alpha_p$')
+    plt.plot(T[0,start:end], np.transpose(z_k[0,start:end]-XX_k1k1[3,start:end]), label=r'$\alpha_{corrected}$')  # todo; check if correct
+    plt.xlabel(r'$Time\,[s]$')
+    plt.ylabel(r'$\alpha\,[rad]$')
+
+    # polyfit
+    DEGREE = 2
+    V_poly = np.polyfit(T[0, start:end], np.transpose(z_pred[0, start:end]), DEGREE)
+    V_poly_f = np.poly1d(V_poly)
+    plt.plot(V_poly_f(np.arange(0, 100, 1)), label='polyfit')
+    plt.legend()
+    plt.show()
+
+    plt.figure(dpi=300)
+    plt.title(r'$Predicted\,vs\,Measured\,Output$')
+    plt.plot(T[0, start:end], np.transpose(z_k[1, start:end]), 'g', label=r'$\beta_m$')
+    plt.plot(T[0, start:end], np.transpose(z_pred[1, start:end]), color='r', linewidth=2, label=r'$\beta_p$')
+    plt.xlabel(r'$Time\,[s]$')
+    plt.ylabel(r'$\beta\,[rad]$')
+
+    # polyfit
+    DEGREE = 2
+    V_poly = np.polyfit(T[0, start:end], np.transpose(z_pred[1, start:end]), DEGREE)
+    V_poly_f = np.poly1d(V_poly)
+    plt.plot(V_poly_f(np.arange(0, 100, 1)), label='polyfit')
+    plt.legend()
+    plt.show()
+
+    plt.figure(dpi=300)
+    plt.title(r'$Predicted\,vs\,Measured\,Output$')
+    plt.plot(T[0, start:end], np.transpose(z_k[2, start:end]), 'g', label=r'$V_m$')
+    plt.plot(T[0, start:end], np.transpose(z_pred[2, start:end]), color='r', linewidth=2, label=r'$V_p$')
+    plt.xlabel(r'$Time\,[s]$')
+    plt.ylabel(r'$V\,[rad]$')
+
+    # polyfit
+    DEGREE = 2
+    V_poly = np.polyfit(T[0, start:end], np.transpose(z_pred[2, start:end]), DEGREE)
+    V_poly_f = np.poly1d(V_poly)
+    plt.plot(V_poly_f(np.arange(0, 100, 1)), label='polyfit')
+    plt.legend()
+    plt.show()
+
+
+    # plt.figure(dpi=300)
+    # plt.title('Measured data')
+    # plt.plot(T[0, start:end], alpha_m[start:end])
+    # plt.show()
+
+# generate train, val, test data
+PERCENTAGE_VALIDATION = 0.2
+
+alpha_corrected = z_k[0,0:-1]-XX_k1k1[3,0:-1]  # todo; this or c_p
+beta_p = z_pred[1, 0:-1]
+cm_a = XX_k1k1[3, 0:-1]
+data = [alpha_corrected, beta_p, cm_a]
+np.savetxt("data.csv", data, delimiter=",")
---
+++ nn_rbf.py
@@ -0,0 +1,166 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(1)
+
+
+def radial_basis_function(x, a, c):
+    v = (x - c) ** 2
+    # v = np.square(w) * (x - c)**2
+    return a * np.exp(-v)
+
+
+def diff_radial_basis_function(x, a, c):
+    v = (x - c) ** 2
+    # v = np.square(w) * (x - c)**2)
+    return -2*a*(x-c)*np.exp(-v)
+
+
+def least_squares(desired_output, current_output):
+    return 0.5 * np.square(desired_output - current_output)
+
+
+def evaluate_error(net, desired_output, current_output):
+    if net.loss_function == least_squares:
+        error = least_squares(desired_output, current_output)
+    else:
+        raise Exception("Loss function not defined")
+    return error
+
+
+def levenberg_marquardt(damping, jacobian, input_weights, error, type):
+    weight_change = np.linalg.inv(jacobian.T.dot(jacobian) + damping*np.eye(len(jacobian))).dot(jacobian.T*error)
+    if type:
+        output_weights = input_weights.ravel() - weight_change
+        output_weights = np.array(np.split(output_weights, 2))
+    else:
+        output_weights = (input_weights.T - weight_change).T
+    return output_weights
+
+
+def train_neural_network(net, input, desired_output):
+    """Trains neural networks
+    input, desired_ouput are arrays of inputs and the ground truth / output
+    """
+    current_epoch = 0
+    current_error = 1E3
+    # input_weights = net.input_weights
+    # output_weights = net.output_weights
+
+    while current_epoch <= net.max_epochs:
+        if current_error > net.goal:
+            for i in range(len(input)):
+                current_input = input[i]
+                current_desired_output = desired_output[i]
+                if net.activation_function == radial_basis_function:
+                    if net.optimizer == levenberg_marquardt:
+                        current_output, current_output_hidden_layer = net.evaluate(current_input)
+                        current_error = evaluate_error(net, current_desired_output, current_output)
+                        # print('Current input is ', current_input)
+                        # print('Current output is ', current_output)
+                        # print('Desired output is ', desired_output)
+                        # print('Error is ', current_error)
+                        input_jacobian, output_jacobian = net.get_jacobian(current_input, current_desired_output)
+                        new_input_weights = levenberg_marquardt(LM_DAMPING, input_jacobian, net.input_weights, error=current_error, type = True)
+                        new_output_weights = levenberg_marquardt(LM_DAMPING, output_jacobian, net.output_weights, error=current_error, type = False)
+                        # print('Inputs ', net.input_weights, new_input_weights)
+                        # print('Outputs ', net.output_weights, new_output_weights)
+                        net.input_weights = new_input_weights
+                        net.output_weights = new_output_weights
+                        print('Epoch ', current_epoch, ' done.')
+                        print(current_error)
+                        current_epoch += 1
+        else:
+            break
+
+
+class NeuralNetwork:
+    """Defines all parameters of the neural network"""
+    def __init__(self, number_of_inputs, number_of_hidden_neurons, number_of_outputs, input_bias_weights,
+                 output_bias_weights, range, max_epochs, goal, min_gradient, learning_rate, activation_function,
+                 optimizer, loss_function, input_weights, output_weights, centers, batch_size):
+        self.number_of_inputs = number_of_inputs
+        self.number_of_hidden_neurons = number_of_hidden_neurons
+        self.number_of_outputs = number_of_outputs
+        self.input_bias_weights = input_bias_weights
+        self.output_bias_weights = output_bias_weights
+        self.range = range
+        self.max_epochs = max_epochs
+        self.goal = goal
+        self.min_gradient = min_gradient
+        self.learning_rate = learning_rate
+        self.activation_function = activation_function
+        self.optimizer = optimizer
+        self.loss_function = loss_function
+        self.input_weights = input_weights
+        self.output_weights = output_weights
+        self.centers = centers
+        self.batch_size = batch_size
+
+    def evaluate(self, current_input):
+        current_output = np.zeros(self.number_of_outputs)
+        current_output_hidden_layer = np.zeros(self.number_of_hidden_neurons)
+        for i in range(self.number_of_inputs):
+            for j in range(self.number_of_hidden_neurons):
+                for k in range(self.number_of_outputs):
+                    xi = current_input[i]
+                    yi = xi  # linear node
+                    xj = yi * self.input_weights[i, j]
+                    if self.activation_function == radial_basis_function:
+                        yj = radial_basis_function(xj, 1, self.centers[j])  # todo; set a, check if centers should be 2D
+                    else:
+                        raise Exception("Activation function not defined")
+                    current_output_hidden_layer += yj  # todo; what is output_hidden_layer used for in the Jacobian?
+                    xk = yj * self.output_weights[j, k]
+                    yk = xk  # linear node
+                    current_output += yk
+        return current_output, current_output_hidden_layer
+
+    def get_jacobian(self, current_input, desired_output):
+        jacobian_input_temp = np.zeros((len(self.input_weights), len(self.input_weights.T)))
+        # jacobian_input=np.zeros((len(self.input_weights) * len(self.input_weights.T)))
+        jacobian_output = np.zeros((len(self.output_weights) * len(self.output_weights.T)))
+        output, output_hidden_layer = self.evaluate(current_input)
+        for i in range(self.number_of_inputs):
+            for j in range(self.number_of_hidden_neurons):
+                jacobian_output[j] = -(desired_output - output) * output_hidden_layer[j]
+                # [w11, w12, w13, w21, w22, w23]
+                for k in range(self.number_of_outputs):  # which is just one in our case
+                    # 0 through 5
+                    temp1 = (-(desired_output - output)[k])
+                    temp2 = temp1 * self.output_weights[j, k]
+                    temp3 = temp2 * diff_radial_basis_function(current_input[i], 1, self.centers[j])  # todo; set a
+                    jacobian_input_temp[i, j] = temp3 * current_input[i]
+        jacobian_input = jacobian_input_temp.ravel()
+        return jacobian_input, jacobian_output
+
+
+MAX_EPOCHS = 10000
+GOAL = 0.001
+MIN_GRADIENT = 0
+LEARNING_RATE = 0.001
+LM_DAMPING = 0.0001
+NUMBER_OF_HIDDEN_NEURONS = 3
+BATCH_SIZE = 1
+
+NUMBER_OF_INPUTS = 2
+NUMBER_OF_OUTPUTS = 1
+
+# weights initialization
+INPUT_WEIGHTS_INIT = np.random.rand(NUMBER_OF_INPUTS, NUMBER_OF_HIDDEN_NEURONS)
+OUTPUT_WEIGHTS_INIT = np.random.rand(NUMBER_OF_HIDDEN_NEURONS, NUMBER_OF_OUTPUTS)
+CENTERS_INIT = np.random.rand(NUMBER_OF_HIDDEN_NEURONS)
+
+rbf_network = NeuralNetwork(number_of_inputs=NUMBER_OF_INPUTS, number_of_hidden_neurons=NUMBER_OF_HIDDEN_NEURONS,
+                            number_of_outputs=NUMBER_OF_OUTPUTS, input_bias_weights=0, output_bias_weights=0, range=0,
+                            max_epochs=MAX_EPOCHS, goal=GOAL, min_gradient=MIN_GRADIENT, learning_rate=LEARNING_RATE,
+                            activation_function=radial_basis_function, optimizer=levenberg_marquardt,
+                            loss_function=least_squares, input_weights=INPUT_WEIGHTS_INIT,
+                            output_weights=OUTPUT_WEIGHTS_INIT, centers=CENTERS_INIT, batch_size=BATCH_SIZE)
+
+# load data
+data = np.genfromtxt('data.csv', delimiter=',')
+
+train_neural_network(rbf_network, [data[0], data[1]], data[2])
+print(rbf_network.input_weights, rbf_network.output_weights)
+print(rbf_network.evaluate([data[0][2], data[1][2]]), data[2][0])
