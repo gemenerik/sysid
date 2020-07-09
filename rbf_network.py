f7dad206a56988030c1d9cbcee5c5a3389203ae5
rik <rikjbouwmeester@gmail.com>
2020-07-09 12:24:11 +0200

    plot grid, compatibility with nn_batches

--- rbf_network.py
+++ rbf_network.py
@@ -24,7 +24,7 @@ rbf_network = NeuralNetwork(number_of_inputs=NUMBER_OF_INPUTS, number_of_hidden_
                             activation_function=radial_basis_function, optimizer=levenberg_marquardt,
                             loss_function=least_squares, input_weights=INPUT_WEIGHTS_INIT,
                             output_weights=OUTPUT_WEIGHTS_INIT, centers=CENTERS_INIT, batch_size=BATCH_SIZE,
-                            damping=LM_DAMPING)
+                            damping=LM_DAMPING, diff_activation_function=diff_radial_basis_function)
 
 # load data
 train_data = np.genfromtxt('train.csv', delimiter=',')
@@ -35,9 +35,11 @@ train_error_log, test_error_log, final_epoch = train_neural_network(rbf_network,
 plt.figure()
 plt.plot(range(final_epoch), train_error_log, label='Train error')
 plt.plot(range(final_epoch), test_error_log, label='Test error')
-plt.xlabel('Epoch')  # unit is number
-plt.ylabel('RMS error')  # todo; unit is same as cmalpha (so unitless?)
+plt.xlabel('Epoch')
+plt.ylabel('RMS error [-]')
+plt.grid()
 plt.legend()
 plt.show()
 
 # todo; plot results in time; approximation vs realll
+# todo; update centers ?
