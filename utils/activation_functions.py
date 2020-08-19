af8cb5ee16b9c69c6574f1006f1e0c8b87a40a84
rik <rikjbouwmeester@gmail.com>
2020-08-19 10:27:57 +0200

    Updated activation functions, added alternative method to calculate differential radial basis function

--- utils/activation_functions.py
+++ utils/activation_functions.py
@@ -13,12 +13,16 @@ def diff_radial_basis_function(x, a, c):
     return -2*a*(x-c)*exp(-v)
 
 
+def alt_diff_radial_basis_function(x, a, c):
+    rad = radial_basis_function(x, a, c)
+    return - rad
+
+
 def sigmoid(x, a, c):
-    v = (x - c) ** 2
+    # v = (x - c) ** 2
+    v = x
     return 2/(1+exp(-2*v))-1
 
 
 def diff_sigmoid(x, a, c):
-    # v = (x - c) ** 2
-    # return 8*v*np.exp(2*np.square(v))
-    return sigmoid(x, a, c) * (1 - sigmoid(x, a, c))
+    return 4 * 1/(1+exp(-2*x)) * (1 - 1/(1+exp(-2*x)))
